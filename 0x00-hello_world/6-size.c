ba7468b9bf9a5547dbfbea5eab42ea360e0efa59
eXochus <odunolaridwan80m@gmail.com>
2023-12-14 14:58:32 -0800

    working with C low level pro

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main(void)
 
 	printf("Size of a char: %c byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %e byte(s)\n", sizeof(float));
 
 	return (0);
 }
