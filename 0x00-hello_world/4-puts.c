4a4912b9f71e29904faa8d38f993a6316c35610e
eXochus <odunolaridwan80m@gmail.com>
2023-12-14 14:23:00 -0800

    working with C low level pro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point for start of the programme
+ *
+ * Return: interger 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
