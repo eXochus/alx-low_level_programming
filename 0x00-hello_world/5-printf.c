f9488e2a043a561b280fc009f0786e081c93d7b5
eXochus <odunolaridwan80m@gmail.com>
2023-12-14 14:29:51 -0800

    working with C low level pro

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for start of the programme
+ *
+ * Return: interger 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
