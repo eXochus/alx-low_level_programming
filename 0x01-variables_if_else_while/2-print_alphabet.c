0d785fd76b62089e7491103ec207c8a63cea5beb
eXochus <odunolaridwan80m@gmail.com>
2023-12-15 13:12:26 -0800

    tackling variables, cond statemnt and loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - defines the entry point of the code
+ *
+ * Return: nothing except 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
