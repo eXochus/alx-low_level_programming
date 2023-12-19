e273a73f0e1c917e6783a3e06b564c018900a3f5
eXochus <odunolaridwan80m@gmail.com>
2023-12-19 10:24:57 -0800

    dealing with custom made functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a to z 10x
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
