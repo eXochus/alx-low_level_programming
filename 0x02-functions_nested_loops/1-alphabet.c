9fa9347d54554a27d6576d1024aab181e12aab2a
eXochus <odunolaridwan80m@gmail.com>
2023-12-19 09:41:40 -0800

    dealing with custom made functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets in lower case
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+	return ;
+}
