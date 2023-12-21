4b952a48f9ceac4f94d8b1cd35fed4e586d039a2
eXochus <odunolaridwan80m@gmail.com>
2023-12-21 06:43:09 -0800

    finally fix its complex

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -20,19 +20,24 @@ int _isalpha(int c)
 			break;
 		}
 		else
-		{
-			for (i = 65; i <= 90; i++)
-			{
-				if (i == c)
-				{
-					result = 1;
-					break;
-				}
-				else
-					result = 0;
-			}
-		}
 			result = 0;
+	}
+	if (result == 1)
+	{
+		result = 1;
+	}
+	else
+	{
+		for (i = 65; i <= 90; i++)
+		{
+			if (i == c)
+			{
+				result = 1;
+				break;
+			}
+			else
+				result = 0;
+		}
 	}
 	return (result);
 }
