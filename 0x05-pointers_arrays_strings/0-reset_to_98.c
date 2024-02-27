bb789b8afeb50f28440b4ff3f0c7ddbca3e20676
eXochus <odunolaridwan80m@gmail.com>
2024-02-27 13:42:00 -0800

    just finding ways

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes adress of array and update
+ * @n: the parameter to be reset
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	return;
+}
