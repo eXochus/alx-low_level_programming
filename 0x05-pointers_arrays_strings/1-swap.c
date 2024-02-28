143e2bd7b9dd94c1a653c138dc3881582ec5704b
eXochus <odunolaridwan80m@gmail.com>
2024-02-28 03:19:14 -0800

    stepper

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_int - takes adress of array and update
- * @n: the first parameter to be swapped
+ * @a: the first parameter to be swapped
  * @b: the second parameter to be swapped
  *
  * Return: nothing
