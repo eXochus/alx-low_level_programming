9f640ed6f464eddba33c1748c4c40f9f7e4cc347
eXochus <odunolaridwan80m@gmail.com>
2024-02-29 02:48:10 -0800

    testing theory

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,13 +9,13 @@
 
 int _strlen(char *s)
 {
-	char len[];
+	char len[20];
 	int i;
 	int count;
 
 	count = 0;
 	len = *s;
-	for (i = s; i == '\0'; i++)
+	for (i = len[0]; i == '\0'; i++)
 		count++;
 
 	return (count);
