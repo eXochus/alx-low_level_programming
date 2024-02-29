dbfa242916f74d4aad2ea017d29caac0c6a3781c
eXochus <odunolaridwan80m@gmail.com>
2024-02-29 07:18:19 -0800

    testing the reverse case

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev- print strings to the stdout in reverse order
+ * @s: the string to be printed
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	char *i;
+
+	for (i = '\0'; *i != s; i--)
+		_putchar(*i);
+	_putchar('\n');
+}
