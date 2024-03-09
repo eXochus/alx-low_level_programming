cbb73610a8bcd142ba04ebcfc11275594b9a6527
eXochus <odunolaridwan80m@gmail.com>
2024-03-09 11:41:29 +0100

    handling the even first

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string of a given variable
+ * @str: the string to be printed in half
+ *
+ * Return: returns nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int lenght_of_str;
+
+	i = 0;
+	lenght_of_str = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+		lenght_of_str++;
+	}
+	if (lenght_of_str % 2 == 0)
+	{
+		for (i = lenght_of_str / 2; i != lenght_of_str; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = 0; i != (lenght_of_str - 1) / 2; i++)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ void reset_to_98(int *n);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
