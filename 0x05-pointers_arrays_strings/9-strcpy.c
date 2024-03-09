71c424afab7de3f9946fe67e779ebd411dda230c
eXochus <odunolaridwan80m@gmail.com>
2024-03-09 18:59:48 +0100

    handling the even first

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string from a pointer to a buffer
+ * @dest: buffer to be copied to
+ * @src: source of the string to be copied
+ *
+ * Return: dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
