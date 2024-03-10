eb6fa577a9a501ed36edddedfafd3f390a4c898e
eXochus <odunolaridwan80m@gmail.com>
2024-03-10 17:44:59 +0100

    replicating standard libary

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,17 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
+	int j;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
-		dest[i] = src[i];
+		i++;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
 		i++;
 	}
 	dest[i] = '\0';
