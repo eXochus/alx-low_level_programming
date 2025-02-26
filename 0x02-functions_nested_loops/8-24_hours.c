0d3a883d697b02c84e0e53d85b05133bfabbdbde
eXochus <odunolaridwan80m@gmail.com>
2025-02-26 12:51:07 +0100

    fixing hours of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,15 +11,16 @@ void jack_bauer(void)
 	int hours;
 	int minutes;
 
-	for (hours = 0; hours <= 24; hours++)
+	for (hours = 0; hours < 24; hours++)
 	{
-		for (minutes = 0; minutes <= 60; minutes++)
+		for (minutes = 0; minutes < 60; minutes++)
 		{
 			_putchar('0' + (hours / 10));
 			_putchar('0' + (hours % 10));
 			_putchar(':');
 			_putchar('0' + (minutes / 10));
 			_putchar('0' + (minutes % 10));
+			_putchar('\n');
 		}
 	}
 }
