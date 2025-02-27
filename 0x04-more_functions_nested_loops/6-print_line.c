9a366c76022e43fd2c1d6ef1587fc870257b8c28
eXochus <odunolaridwan80m@gmail.com>
2025-02-27 21:29:17 +0100

    adding lines without space

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print dashes nx
+ * @n: the number of dashes to be printe
+ *
+ * Return: returns nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
