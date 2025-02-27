cb73757e259255d27c89647e2479a6ee1ec95858
eXochus <odunolaridwan80m@gmail.com>
2025-02-27 17:05:04 +0100

    more than they ascii

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - write numbers from 0 to 14 10x 
+ *
+ * Return: returns nothing
+ */
+
+void more_numbers(void)
+{
+	int number;
+	int times;
+
+	for (times = 0; times < 10 ; times++)
+	{
+		for (number = 0; number < 15; number++)
+		{
+			if (number >= 10)
+				_putchar('0' + (number / 10));
+			_putchar('0' + (number % 10));
+		}
+		_putchar('\n');
+	}
+}
