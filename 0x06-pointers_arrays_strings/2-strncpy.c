cf97293efbf786a830b9b12b5ea68c6d073dc762
eXochus <odunolaridwan80m@gmail.com>
2025-03-05 12:59:48 +0100

    fixing the copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
-
-	for (j = 0; src[j] != '\0'; j++)
-		;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	if (n >= j)
+	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
 }
