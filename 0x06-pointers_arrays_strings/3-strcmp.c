be4af91e4fb16477bd28b3d86b31e687d669c402
eXochus <odunolaridwan80m@gmail.com>
2025-03-05 17:36:06 +0100

    copy cat

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j;
-	int result;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	if (s1[i - 1] == s2[j - 1])
-		result = 0;
-	else
-		result = s2[j - 1] - s1[i - 1];
-	return (result);
+	for (i = 0; s1[i] == '\0' && s2[i] == '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (s1[i] - s2[i]);
 }
