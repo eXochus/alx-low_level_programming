8019a90ebce8f9ef9a8275f07df2075f4d00bd10
eXochus <odunolaridwan80m@gmail.com>
2025-03-18 12:58:40 +0100

    finding mimo

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - finds the first occurence of a character
+ * @s: string to find the character from
+ * @c: character to find
+ *
+ * Return: pointer to that character.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (0);
+}
