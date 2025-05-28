85a680b1a27dd7fc4e921089155d333a8994e451
eXochus <odunolaridwan80m@gmail.com>
2025-05-28 18:36:38 +0100

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,3 +11,21 @@ int _sqrt_recursion(int n)
 {
 	return sqrt_helper(n, 1, 0);
 }
+/**
+ * sqrt_helper - does the job properly by allowing more parameters
+ * @n: the number whose square root is to be calculated
+ * @odd_sub: the odd numbers to subtract
+ * @count: The tabs of how many subs we've made
+ *
+ * Return: the calculated result.
+ */
+
+int sqrt_helper(int n, int odd_sub, int count)
+{
+        n =+ n - odd_sub;
+        if (n == 0)
+                return (count + 1);
+        if (n <= -1)
+                return (-1);
+        return sqrt_helper(n, odd_sub + 2, count + 1);
+}
