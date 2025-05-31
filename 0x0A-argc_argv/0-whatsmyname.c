1f71cc96d7ef3060b1868ac29bb803df0f74190a
eXochus <odunolaridwan80m@gmail.com>
2025-05-31 18:53:45 +0100

    tesing arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the  name of the argument
+ * @argc: number of arguments
+ * @argv: arrays of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
