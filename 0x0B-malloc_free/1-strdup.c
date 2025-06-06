415ef4e023dc14e8fcfb24b9aa83214a1e6645fe
eXochus <odunolaridwan80m@gmail.com>
2025-06-06 10:48:07 +0100

    copying without numbers

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - create a copy of a given array
+ * @str: the given array
+ *
+ * Return: pointer array
+ */
+
+char *_strdup(char *str)
+{
+	char *create;
+	int size;
+	int count;
+
+	size = _strlen(str);
+	if (size == 0)
+		return (NULL);
+	create = malloc(sizeof(char) * size);
+	for (count = 0; count <= size; count++)
+		create[count] = str[count];
+	return (create);
+}
+
+/**
+ * _strlen - counts the length of a string
+ * @s: the string to be counted
+ *
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	char *i;
+	int count;
+
+	count = 0;
+	for (i = s; *i != '\0'; i++)
+		count++;
+
+	return (count);
+
+}
