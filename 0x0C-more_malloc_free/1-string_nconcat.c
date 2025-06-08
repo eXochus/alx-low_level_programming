7c1bc30432a051ab10badac4650564c6e0218bda
eXochus <odunolaridwan80m@gmail.com>
2025-06-08 20:45:02 +0100

    more mallocs

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,6 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		concat[count1] = s1[count1];
 	for (count2 = 0; count2 < n; count2++, count1++)
 		concat[count1] = s2[count2];
+	concat[count1] = '\0';
 	return (concat);
 }
 
