7a48f9f60885d8e0ab3e58305e29157f05ff8818
eXochus <odunolaridwan80m@gmail.com>
2025-06-17 16:55:45 +0100

    modifying with typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: the owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+} dog_t;
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 #endif /* _DOG_H_ */
