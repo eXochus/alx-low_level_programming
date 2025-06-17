652d71f8a6bda28512c48ef9ec5f8ddc17aa1ed2
eXochus <odunolaridwan80m@gmail.com>
2025-06-17 14:33:47 +0100

    structuring defined type

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Type defination of Structured dt
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
