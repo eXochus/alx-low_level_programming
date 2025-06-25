afceb4c3d46f305e26e6bd9ad2f4a97934656ffe
eXochus <odunolaridwan80m@gmail.com>
2025-06-25 09:29:29 +0100

    took more than 7 days to heal:

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - creates new dog from the given parameters
@@ -12,30 +11,51 @@
  * Return: dog instance
  */
 
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	for (len = 0; src[len] != '\0'; len++)
+		dest[len] = src[len];
+	return (dest);
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *the_dog;
 	char *namem;
 	char *ownerm;
+	int namel;
+	int ownerl;
 
 	the_dog = malloc(sizeof(dog_t));
 	if (the_dog == NULL)
 		return (NULL);
-	namem = malloc(sizeof(char) * strlen(name) + 1);
+	namel = _strlen(name);
+	namem = malloc(sizeof(char) * namel + 1);
 	if (namem == NULL)
 	{
 		free(the_dog);
 		return (NULL);
 	}
-	ownerm = malloc(sizeof(char) * strlen(owner) + 1);
+	ownerl = _strlen(owner);
+	ownerm = malloc(sizeof(char) * ownerl + 1);
 	if (ownerm == NULL)
 	{
 		free(the_dog);
 		free(namem);
 		return (NULL);
 	}
-	namem = strcpy(namem, name);
-	ownerm = strcpy(ownerm, owner);
+	namem = _strcpy(namem, name);
+	ownerm = _strcpy(ownerm, owner);
 	the_dog->name = namem;
 	the_dog->age = age;
 	the_dog->owner = ownerm;
