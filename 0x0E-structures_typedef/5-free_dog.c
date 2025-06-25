e569fa2da07d72910daf02b5bcb6d578610d60c9
eXochus <odunolaridwan80m@gmail.com>
2025-06-25 10:32:41 +0100

    tesing val

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog
+ * @d: instance of the dog to be freed
+ *
+ * Return: the len
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
