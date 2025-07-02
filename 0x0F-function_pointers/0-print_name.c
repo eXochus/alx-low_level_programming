4eb0de5e1824329f826ef4a8f2be2ffaebb41f38
eXochus <odunolaridwan80m@gmail.com>
2025-07-02 14:38:57 +0100

    checkers requirement

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - prints the name passed into it
@@ -10,5 +11,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
