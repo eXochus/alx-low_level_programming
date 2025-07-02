3686e2fdd42d59de5ded5566dbe37227eda3f710
eXochus <odunolaridwan80m@gmail.com>
2025-07-02 15:22:06 +0100

    aplying fun to arrays

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - apply called function on each element of given array
+ * @array: array to be given
+ * @size: size of the array
+ * @action: the function to be called
+ *
+ * Return: returns nothing
+ */
+
+void array_iterator(int *array,size_t size, void (*action)(int))
+{
+	size_t counter;
+
+	if (action != NULL)
+	{
+		for (counter = 0; counter < size; counter++)
+			action(array[counter]);
+	}
+}
