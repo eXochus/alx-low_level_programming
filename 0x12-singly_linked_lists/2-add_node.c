4ce503b0f4c614cde8e71a53879e060608ffe24d
eXochus <odunolaridwan80m@gmail.com>
2025-09-02 12:29:02 +0100

    fixing upnext issues

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,13 +6,18 @@
 /**
  * add_node - add node the beginning of a list
  * @head: the initial beginning
- * @:str: the content of the new node
+ * @str: the content of the new node
  *
  * Return: the address of the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
